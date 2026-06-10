1e005f98d9ad1d0a137368bbef62bab3dae93113
Web <webd75369@gmail.com>
2026-06-11 01:13:33 +0530

    added custom errors and response classes

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -2,12 +2,16 @@ import "dotenv/config";
 import express, { Application, Request, Response } from "express";
 import logger from "./config/logger";
 import { env } from "./config/env";
+import { ApiResponse } from "./utils/response";
+import StatusCodes from "http-status-codes";
 
 const app: Application = express();
 const port = env.port || 3000;
 
 app.get("/health", (_req: Request, res: Response) => {
-  return res.status(200).json({ message: "api is running" });
+  return res
+    .status(200)
+    .json(new ApiResponse(StatusCodes.OK, true, "api is working"));
 });
 
 app.listen(port, () => {

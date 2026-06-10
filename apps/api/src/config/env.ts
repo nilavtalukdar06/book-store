6b82e332b13ccab04b86bda93eb0027409dd06cc
Web <webd75369@gmail.com>
2026-06-11 00:46:10 +0530

    added logger

--- apps/api/src/config/env.ts
+++ apps/api/src/config/env.ts
@@ -5,6 +5,7 @@ dotenv.config();
 
 const envSchema = z.object({
   databaseUrl: z.string().url(),
+  port: z.number().default(3000),
 });
 
 const env = envSchema.parse(process.env);
--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,14 +1,15 @@
+import "dotenv/config";
 import express, { Application, Request, Response } from "express";
-import dotenv from "dotenv";
-dotenv.config();
+import logger from "./config/logger";
+import { env } from "./config/env";
 
 const app: Application = express();
-const port = process.env.PORT || 3000;
+const port = env.port || 3000;
 
-app.get("/", (_req: Request, res: Response) => {
+app.get("/health", (_req: Request, res: Response) => {
   return res.status(200).json({ message: "api is running" });
 });
 
 app.listen(port, () => {
-  console.log("server is running on port", port);
+  logger.info("server is running on port", port);
 });

d005d8bef2384cd1422eaba495974fecf0506868
Web <webd75369@gmail.com>
2026-06-12 11:09:53 +0530

    mounted auth routes to the server

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -3,12 +3,14 @@ import express, { Application, Request, Response } from "express";
 import logger from "./config/logger";
 import { ApiResponse } from "./utils/response";
 import StatusCodes from "http-status-codes";
+import authRoutes from "./routes/auth";
 
 const app: Application = express();
 const port = env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/api/auth", authRoutes);
 
 app.get("/health", (_req: Request, res: Response) => {
   return res

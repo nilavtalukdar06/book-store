59d22bbfd491562553e366a12d58cfcf5ace5fd2
nilavtalukdar06 <nilavtalukdar06@gmail.com>
2026-06-12 21:24:17 +0530

    added test use effect

--- apps/mobile/app/index.tsx
+++ apps/mobile/app/index.tsx
@@ -1,6 +1,17 @@
+import { useEffect } from "react";
 import { Text, View } from "react-native";
 
 export default function Index() {
+  useEffect(() => {
+    const fetchApi = async () => {
+      const response = await fetch(
+        "https://book-store-api-cyan.vercel.app/health",
+      );
+      const result = await response.json();
+      console.log(result);
+    };
+    fetchApi();
+  }, []);
   return (
     <View
       style={{

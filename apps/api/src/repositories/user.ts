56891f3dd256cfe3d74a7196ee236530acd320cd
Web <webd75369@gmail.com>
2026-06-12 09:25:16 +0530

    added user repository

---
+++ apps/api/src/repositories/user.ts
@@ -0,0 +1,23 @@
+import { PrismaClient, User } from "../generated/prisma/client";
+
+export abstract class UserRepository {
+  private prisma: PrismaClient;
+  constructor(prisma: PrismaClient) {
+    this.prisma = prisma;
+  }
+  async findByEmail(email: string) {
+    return this.prisma.user.findUnique({
+      where: { email },
+    });
+  }
+  async createUser(data: Omit<User, "id" | "createdAt" | "updatedAt">) {
+    return this.prisma.user.create({
+      data,
+    });
+  }
+  async findById(id: string) {
+    return this.prisma.user.findUnique({
+      where: { id },
+    });
+  }
+}

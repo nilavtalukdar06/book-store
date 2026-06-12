3237a9efa8f93d69ce8c44a72ee5efe4327c0a82
Web <webd75369@gmail.com>
2026-06-12 14:35:25 +0530

    added cloudinary config

--- apps/api/src/config/env.ts
+++ apps/api/src/config/env.ts
@@ -6,4 +6,7 @@ export const env = {
   PORT: process.env.PORT!,
   DATABASE_URL: process.env.DATABASE_URL!,
   JWT_SECRET: process.env.JWT_SECRET!,
+  CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME!,
+  CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY!,
+  CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET!,
 };

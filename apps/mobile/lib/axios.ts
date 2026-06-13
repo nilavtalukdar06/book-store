cdec6823e987619583b2425a300769e11ac0f87c
nilavtalukdar06 <nilavtalukdar06@gmail.com>
2026-06-13 22:20:37 +0530

    added axios config

---
+++ apps/mobile/lib/axios.ts
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { env } from "../config/env";
+import { SecureStorage } from "./secure-storage";
+
+export const api = axios.create({
+  baseURL: env.API_URL,
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+const secureStorage = new SecureStorage();
+
+api.interceptors.request.use(
+  async (config) => {
+    const token = await secureStorage.getToken();
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    if (error.response?.status === 401) {
+      await secureStorage.removeToken();
+    }
+    return Promise.reject(error);
+  },
+);

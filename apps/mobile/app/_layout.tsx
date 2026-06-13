d7ad43ce525331dcb8e126ac0560c646ef1acc5c
nilavtalukdar06 <nilavtalukdar06@gmail.com>
2026-06-13 23:07:53 +0530

    wrapped the app inside auth provider

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -1,5 +1,6 @@
 import { SafeScreen } from "@/components/safe-screen";
 import { queryClient } from "@/config/query-client";
+import { AuthProvider } from "@/context/auth-context";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
@@ -8,15 +9,17 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 export default function RootLayout() {
   return (
     <QueryClientProvider client={queryClient}>
-      <SafeAreaProvider>
-        <SafeScreen>
-          <Stack screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="index" />
-            <Stack.Screen name="(auth)" />
-          </Stack>
-        </SafeScreen>
-        <StatusBar style="dark" />
-      </SafeAreaProvider>
+      <AuthProvider>
+        <SafeAreaProvider>
+          <SafeScreen>
+            <Stack screenOptions={{ headerShown: false }}>
+              <Stack.Screen name="index" />
+              <Stack.Screen name="(auth)" />
+            </Stack>
+          </SafeScreen>
+          <StatusBar style="dark" />
+        </SafeAreaProvider>
+      </AuthProvider>
     </QueryClientProvider>
   );
 }

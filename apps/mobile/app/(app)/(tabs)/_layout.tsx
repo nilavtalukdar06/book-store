70b44a61cc333beeee3d7c0d326bb4f5077e259f
nilavtalukdar06 <nilavtalukdar06@gmail.com>
2026-06-14 15:15:48 +0530

    added tabs layout

---
+++ apps/mobile/app/(app)/(tabs)/_layout.tsx
@@ -0,0 +1,36 @@
+import { Ionicons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+
+export default function TabsLayout() {
+  return (
+    <Tabs screenOptions={{ headerShown: false }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons color={color} size={size} name="home-outline" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="create"
+        options={{
+          title: "Create",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons color={color} size={size} name="add-circle-outline" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons color={color} size={size} name="person-outline" />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}

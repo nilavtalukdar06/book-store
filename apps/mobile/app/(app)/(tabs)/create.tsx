caf6241af672f2713e2a8de194e920083175442e
nilavtalukdar06 <nilavtalukdar06@gmail.com>
2026-06-14 15:49:05 +0530

    added create screen

--- apps/mobile/app/(app)/(tabs)/create.tsx
+++ apps/mobile/app/(app)/(tabs)/create.tsx
@@ -1,9 +1,39 @@
-import { Text, View } from "react-native";
+import styles from "@/styles/create";
+import { useRouter } from "expo-router";
+import { useState } from "react";
+import { KeyboardAvoidingView, Platform, ScrollView, Text, View } from "react-native";
 
 export default function CreateScreen() {
+  const [title, setTitle] = useState<string>("");
+  const [caption, setCaption] = useState<string>("");
+  const [rating, setRating] = useState<number>(3);
+  const [image, setImage] = useState<string>("");
+  const [imageBase64, setImageBase64] = useState<string>("");
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const router = useRouter();
+
+  const pickImage = async () => {};
+  const handleSubmit = async () => {};
+
   return (
-    <View>
-      <Text>Create Screen</Text>
-    </View>
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <ScrollView
+        contentContainerStyle={styles.container}
+        style={styles.scrollViewStyle}
+      >
+        <View style={styles.card}>
+          <View style={styles.header}>
+            <Text style={styles.title}>Add Book Recommendation</Text>
+            <Text style={styles.subtitle}>
+              Share your favorite reads with others
+            </Text>
+          </View>
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
--- apps/mobile/styles/create.ts
+++ apps/mobile/styles/create.ts
@@ -20,7 +20,7 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.1,
     shadowRadius: 8,
-    elevation: 3,
+    elevation: 0,
     borderWidth: 1,
     borderColor: COLORS.border,
   },

b86d62696f4adf47a5d76e09ac4520cc9098971f
nilavtalukdar06 <nilavtalukdar06@gmail.com>
2026-06-18 01:16:54 +0530

    added book hooks for fetching books

--- apps/mobile/hooks/book-hooks.ts
+++ apps/mobile/hooks/book-hooks.ts
@@ -1,6 +1,10 @@
 import { BookService } from "@/services/book";
 import { BookSchema } from "@/validators/book";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import {
+  useInfiniteQuery,
+  useMutation,
+  useQueryClient,
+} from "@tanstack/react-query";
 import { useRouter } from "expo-router";
 
 export const useCreateBook = () => {
@@ -16,6 +20,22 @@ export const useCreateBook = () => {
         queryKey: ["books"],
       });
       router.replace("/");
+    },
+  });
+};
+
+export const useBooks = () => {
+  return useInfiniteQuery({
+    queryKey: ["books"],
+    initialPageParam: 1,
+    queryFn: async ({ pageParam }) => {
+      return await BookService.getBooks(pageParam, 2);
+    },
+    getNextPageParam: (lastPage) => {
+      const currentPage = lastPage.data?.currentPage;
+      const totalPages = lastPage.data?.totalPages;
+      if (currentPage == null || totalPages == null) return undefined;
+      return currentPage < totalPages ? currentPage + 1 : undefined;
     },
   });
 };

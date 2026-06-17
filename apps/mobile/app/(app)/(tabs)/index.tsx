6ea0cd7988b4a41909ca3b26e78fa846d8e95de7
nilavtalukdar06 <nilavtalukdar06@gmail.com>
2026-06-18 01:00:44 +0530

    added book detail component and added date time helpers

--- apps/mobile/app/(app)/(tabs)/index.tsx
+++ apps/mobile/app/(app)/(tabs)/index.tsx
@@ -1,6 +1,11 @@
 import { useAuth } from "@/context/auth-context";
-import styles from "@/styles/profile";
-import { Text, TouchableOpacity, View } from "react-native";
+import styles from "@/styles/home";
+import { Book } from "@/types/book";
+import { Text, View } from "react-native";
+import { Image } from "expo-image";
+import { Ionicons } from "@expo/vector-icons";
+import { COLORS } from "@/colors/colors";
+import { formatPublishDate } from "@/utils/time";
 
 export default function Index() {
   const { logout } = useAuth();
@@ -14,16 +19,55 @@ export default function Index() {
         justifyContent: "center",
         alignItems: "center",
       }}
-    >
-      <TouchableOpacity style={styles.logoutButton} onPress={handleLogout}>
-        <Text
-          style={{
-            color: "white",
-          }}
-        >
-          Logout
-        </Text>
-      </TouchableOpacity>
-    </View>
+    ></View>
   );
 }
+
+export const RenderItem = ({ item }: { item: Book }) => {
+  return (
+    <View style={styles.bookCard}>
+      <View style={styles.bookHeader}>
+        <View style={styles.userInfo}>
+          <Image
+            source={{ uri: item.user.profileImageUrl }}
+            style={styles.avatar}
+          />
+          <Text style={styles.username}>{item.user.name}</Text>
+        </View>
+      </View>
+      <View style={styles.bookImageContainer}>
+        <Image
+          source={item.imageUrl}
+          style={styles.bookImage}
+          contentFit="cover"
+        />
+      </View>
+      <View style={styles.bookDetails}>
+        <Text style={styles.bookTitle}>{item.title}</Text>
+        <View style={styles.ratingContainer}>
+          <RenderRatings rating={item.rating} />
+        </View>
+        <Text style={styles.caption}>{item.caption}</Text>
+        <Text style={styles.date}>
+          Shared on {formatPublishDate(item.createdAt)}
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+export const RenderRatings = ({ rating }: { rating: number }) => {
+  const stars = [];
+  for (let i = 1; i <= 5; i++) {
+    stars.push(
+      <Ionicons
+        key={i}
+        name={i <= rating ? "star" : "star-outline"}
+        size={16}
+        color={i <= rating ? "#f4b400" : COLORS.textSecondary}
+        style={{ marginRight: 2 }}
+      />,
+    );
+  }
+  return stars;
+};
---
+++ apps/mobile/utils/time.ts
@@ -0,0 +1,7 @@
+export function formatPublishDate(dateString: string) {
+  const date = new Date(dateString);
+  const month = date.toLocaleString("default", { month: "long" });
+  const day = date.getDate();
+  const year = date.getFullYear();
+  return `${month} ${day}, ${year}`;
+}

10d42695ff1bf5be1587169f2bbbed994b209675
nilavtalukdar06 <nilavtalukdar06@gmail.com>
2026-06-18 02:32:29 +0530

    added hooks for books

--- apps/mobile/hooks/book-hooks.ts
+++ apps/mobile/hooks/book-hooks.ts
@@ -4,6 +4,7 @@ import {
   useInfiniteQuery,
   useMutation,
   useQueryClient,
+  useQuery,
 } from "@tanstack/react-query";
 import { useRouter } from "expo-router";
 
@@ -36,6 +37,30 @@ export const useBooks = () => {
       const totalPages = lastPage.data?.totalPages;
       if (currentPage == null || totalPages == null) return undefined;
       return currentPage < totalPages ? currentPage + 1 : undefined;
+    },
+  });
+};
+
+export const useUserBooks = () => {
+  return useQuery({
+    queryKey: ["user-books"],
+    queryFn: async () => {
+      return await BookService.getUserBooks();
+    },
+  });
+};
+
+export const useDelete = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationKey: ["delete-user-books"],
+    mutationFn: async (bookId: string) => {
+      return await BookService.deleteBook(bookId);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["delete-user-books"],
+      });
     },
   });
 };
